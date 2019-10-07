9d0e01d5dc8a44361cc90600776f052b9869d3d5
Vladislav Shevel <vladislav.shevel@gmail.com>
2019-10-08 00:06:22 +0300

    feat:solution for all type of students

--- src/index.js
+++ src/index.js
@@ -7,8 +7,48 @@
 module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
-    config = {family: 4}
+    config = {family: 4, friends: 10, normal_life: 20, profession: 30, carrier: 40, top_peformance: 60}
     ) {
-      return 0;
+      if(knowsProgramming){
+      if (focus == 'family'){
+      return Math.ceil(800 / config.family);
+      }
+      if(focus == 'friends'){
+        return Math.ceil(800 / config.friends);
+      }
+      if(focus == 'normal_life'){
+        return Math.ceil(800 / config.normal_life);
+      }
+      if(focus == 'profession'){
+        return Math.ceil(800 / config.profession);
+      }
+      if(focus == 'carrier'){
+        return Math.ceil(800 / config.carrier);
+      }
+      if(focus == 'top_peformance'){
+        return Math.ceil(800 / config.top_peformance);
+      } 
+    }
+    else if (!knowsProgramming){
+      if (focus == 'family'){
+      return Math.ceil(1300 / config.family);
+      }
+      if(focus == 'friends'){
+        return Math.ceil(1300 / config.friends);
+      }
+      if(focus == 'normal_life'){
+        return Math.ceil(1300 / config.normal_life);
+      }
+      if(focus == 'profession'){
+        return Math.ceil(1300 / config.profession);
+      }
+      if(focus == 'carrier'){
+        return Math.ceil(1300 / config.carrier);
+      }
+      if(focus == 'top_peformance'){
+        return Math.ceil(1300 / config.top_peformance);
+      } 
+    }
+
   };
   
